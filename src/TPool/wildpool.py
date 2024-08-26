a3b5559ab8293ad512d8c85af4da5f25fd9b5294
Ahmad Alobaid <ahmad88me@gmail.com>
2024-08-26 20:48:00 +0200

    rewrote wildpool to use locks and blocks instead of polling. Which means better performance. Also resources are being released after the deletion of the object

--- src/TPool/wildpool.py
+++ src/TPool/wildpool.py
@@ -1,6 +1,7 @@
 import threading
 import logging
 import time
+import queue
 
 
 class WildPool:
@@ -11,157 +12,117 @@ class WildPool:
     :type pool_size: int
     :param timeout: The default timeout for threads if not specified.
     :type timeout: int
-    :param worker_timeout: The idle timeout for the worker.
-    :type worker_timeout: int
     :param logger: Optional custom logger. If not provided, a default logger will be used.
     :type logger: logging.Logger
     """
-    def __init__(self, pool_size=5, timeout=0, worker_timeout=3, logger=None):
+    def __init__(self, pool_size=5, logger=None):
         """
         pool_size: int
         timeout: int in seconds
-        worker_timeout: int in seconds
         logger: The logger.
         """
+        self.semaphore = threading.Semaphore(pool_size)
         self.pool_size = pool_size
         self.pool = dict()
-        self.bench = []
-        self.lock = threading.Lock()
+        self.bench = queue.SimpleQueue()
+        self.queue_lock = threading.Lock()
+        self.worker_lock = threading.Lock()
         self.worker = None
-        self.timeout = timeout
-        self.worker_timeout = worker_timeout
-        self.last_kill = None
+        self.keep_going = True
         if not logger:
             logger = logging.getLogger(__name__)
             logger.setLevel(logging.CRITICAL)
-            # create console handler and set level to debug
             ch = logging.NullHandler()
             logger.addHandler(ch)
         self.logger = logger
 
-    def add_thread(self, thread: threading.Thread, timeout=0):
+    def _should_keep_going(self):
+        with self.worker_lock:
+            keep_going = self.keep_going
+        return keep_going
+
+    def add_thread(self, thread: threading.Thread):
         """
         Add a thread to the pool.
 
         :param thread: The thread to be added to the pool.
         :type thread: threading.Thread
-        :param timeout: The timeout for the thread. If not specified, the default pool timeout will be used.
-        :type timeout: int
         """
-        d = {
-            "thread": thread
-        }
-        if timeout > 0:
-            d["timeout"] = timeout
-        elif self.timeout > 0:
-            d["timeout"] = self.timeout
+        self.bench.put(thread)
 
-        with self.lock:
-            self.bench.append(d)
-        self.start()
+    def _run_in_capsule(self, thread):
+        thread.start()
+        tid = thread.ident
+        with self.queue_lock:
+            self.pool[tid] = thread
+        thread.join()
+        self.semaphore.release()
 
-    def start(self):
-        """
-        Start the worker if there are threads in the bench and no worker is currently running.
-        """
-        with self.lock:
-            wake_up = False
-            if self.bench:
-                self.logger.debug("Bench is not empty")
-                if not self.worker or not self.worker.is_alive:
-                    wake_up = True
-                else:
-                    wake_up = False
-            else:
-                self.logger.debug("Bench is empty")
-        if wake_up:
-            self._create_worker()
+    def _start_capsule(self, thread):
+        th = threading.Thread(target=self._run_in_capsule, args=(thread,))
+        th.start()
 
     def _kick_dead_threads(self):
         """
         Remove threads which are already completed from the Pool so more threads can be added
         """
-        tids = list(self.pool.keys())
-        for tid in tids:
-            t = self.pool[tid]
-            if not t["thread"].is_alive():
-                self.logger.debug(f"Removing thread {t} from the pool")
-                del self.pool[tid]
-                self.last_kill = time.time()
-
-    def _kick_slow_threads(self):
-        """
-        Remove threads which exceeded the specified timeout from the Pool so more threads can be added
-        """
-        tids =list(self.pool.keys())
-        for tid in tids:
-            t = self.pool[tid]
-            if "timeout" in t:
-                elapsed = time.time() - t["start_time"]
-                if elapsed > t["timeout"]:
-                    self.logger.debug(f"Killing thread {t} and removing it from the pool")
-                    t["thread"].kill()
-                    self.last_kill = time.time()
+        with self.queue_lock:
+            tids = list(self.pool.keys())
+            for tid in tids:
+                t = self.pool[tid]
+                if not t.is_alive():
+                    self.logger.debug(f"TPool: Removing thread {t} from the pool")
                     del self.pool[tid]
 
-    def _create_worker(self):
+    def start_worker(self):
         """
-        Spawn a new worker if there is no working running already
+        Spawn a new worker if there is no running one already
         """
-        self.logger.debug("create worker")
-        with self.lock:
-            if self.worker and self.worker.is_alive:
-                self.logger.debug("The worker is already running")
+        self.logger.debug("TPool: create worker")
+        with self.worker_lock:
+            if self.worker and self.worker.is_alive():
+                self.logger.debug("TPool: The worker is already running")
             else:
-                self.logger.debug("Spawning a new worker")
+                self.logger.debug("TPool: Spawning a new worker")
                 self.worker = threading.Thread(target=self._worker_func)
                 self.worker.start()
-                self.logger.debug("worker is ran")
+                self.logger.debug("TPool: worker is running")
 
-    def _clean_pool(self):
-        self._kick_dead_threads()
-        self._kick_slow_threads()
-
-    def _should_break(self):
-        b = False
-        if self.worker_timeout > 0 and self.last_kill:
-            with self.lock:
-                elapsed = time.time() - self.last_kill
-                if elapsed > self.worker_timeout:
-                    b = True
-        return b
-
-    def _fill_the_pool(self):
-        tids = self.pool.keys()
-        num_vacancies = max(self.pool_size - len(tids), 0)
-        num_mov = min(num_vacancies, len(self.bench))
-        for _ in range(num_mov):
-            tj = self.bench.pop(0)
-            tid = tj["thread"].start()
-            if "timeout" in tj:
-                tj["start_time"] = time.time()
-            self.pool[tid] = tj
+    def _jump_into_the_pool(self, thread):
+        if thread:
+            self._start_capsule(thread)
+        else:
+            self.logger.debug(f"TPool: None is passed to jump.")
 
     def _worker_func(self):
-        while True:
-            self.lock.acquire()
-            self._clean_pool()
-            self._fill_the_pool()
-            self.lock.release()
-            if self._should_break():
-                break
+        self.semaphore.acquire()  # This is not a typo. Because otherwise, n+1 will be running instead of n threads
+        while self._should_keep_going():
+            th = self.bench.get()
+            self._jump_into_the_pool(thread=th)
+            self._kick_dead_threads()
+            self.semaphore.acquire()
+        self.logger.debug("TPool: The worker is terminated")
 
-    def join(self, relax=0.1):
-        """
-        Wait for all threads in the pool and bench to complete.
+    def stop(self):
+        self.logger.debug("TPool: releasing resources ...")
+        with self.worker_lock:
+            if self.worker and self.worker.is_alive():
+                self.keep_going = False
+                worker_alive = True
+            else:
+                worker_alive = False
+        self.bench.put(None)  # to release the block in the main loop in case the bench get is blocking
+        try:
+            # to release the semaphore block in case it was blocking. Extra releasing will cause the exception
+            self.semaphore.release()
+        except Exception as e:
+            pass
+        self.logger.debug("TPool: waiting for the worker to join")
+        if worker_alive:
+            self.worker.join()
+            self.logger.debug("TPool: the worker is joined")
+        else:
+            self.logger.debug("TPool: the worker is not running so join would be skipped")
 
-        :param relax: Sleep time in seconds between checks. 0 means no waiting.
-        :type relax: float
-        """
-        done = False
-        while not done:
-            with self.lock:
-                if len(self.pool) == len(self.bench) == 0:
-                    done = True
-            if relax > 0:
-                time.sleep(relax)
+    def __del__(self):
+        self.stop()
